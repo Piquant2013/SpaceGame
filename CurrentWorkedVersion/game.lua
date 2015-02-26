d4d8876a07d9fcf78021788e6c1eb39fb11b00c2
Thomas Wiltshire <thomas.wiltshire14@gmail.com>
2015-02-26 21:29:22 +1100

    Removed Comments
    (Stupid GitHub Code Comments That Broke Game)

--- CurrentWorkedVersion/game.lua
+++ CurrentWorkedVersion/game.lua
@@ -12,15 +12,6 @@ local camera = require "vendors/camera"
 
 
 HC = require 'vendors/hardoncollider'
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
->>>>>>> Thomas-Changes
 pause = require 'pause'
 
 
@@ -28,17 +19,10 @@ pause = require 'pause'
 
 
 
-<<<<<<< HEAD
-function on_collision(dt, shape_a, shape_b)
-end
-
-function collision_stop(dt, shape_a, shape_b)
-=======
 function on_collision(dt, shape_a, shape_b, shape_c)
 end
 
 function collision_stop(dt, shape_a, shape_b, shape_c)
->>>>>>> Thomas-Changes
 end
 
 
@@ -46,10 +30,6 @@ end
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> Thomas-Changes
 function game:init()
 	------ VARIABLES ------
 	-- Player postion
@@ -82,28 +62,21 @@ function game:init()
 
 
 
-<<<<<<< HEAD
-=======
 	BoostTired = false
 	BoostTiredTime = 0 
 	Boost = false
 	BoostTime = 0
 
->>>>>>> Thomas-Changes
 	Tired = false
 	TiredTime = 0 
 	Sprint = false
 	PlayerSpeed = 70
 	SprintTime = 0
 
-<<<<<<< HEAD
-	BackFont = love.graphics.newFont("fonts/xen3.ttf", 50)
-=======
 	InteractFont = love.graphics.newFont("fonts/xen3.ttf", 10)
 	BackFont = love.graphics.newFont("fonts/xen3.ttf", 50)
 	gameovertextfont = love.graphics.newFont("fonts/xen3.ttf", 40)
 	gameoverfont = love.graphics.newFont("fonts/xen3.ttf", 90)
->>>>>>> Thomas-Changes
 	welcomefont = love.graphics.newFont("fonts/xen3.ttf", 20)
 	
 	welcome = true
@@ -116,106 +89,6 @@ function game:init()
 	GameSelect1M = love.audio.newSource("audio/sel.ogg")
 	EnterGame = love.audio.newSource("audio/enter.ogg")
 
-<<<<<<< HEAD
-	Collider = HC(100,on_collision , collision_stop)
-
-	rockcol = true
-	rockx = 800
-	rocky = 300
-	rock = love.graphics.newImage("images/rock.png")
-	rockhitbox = Collider:addRectangle(rockx,rocky,22,22)
-
-	rockcol2 = true
-	rock2x = 500
-	rock2y = 350
-	rock2 = love.graphics.newImage("images/rock.png")
-	rock2hitbox = Collider:addRectangle(rock2x,rock2y,22,22)
-
-	rockcol3 = true
-	rock3x = 100
-	rock3y = 900
-	rock3 = love.graphics.newImage("images/rock.png")
-	rock3hitbox = Collider:addRectangle(rock3x,rock3y,22,22)
-
-	rockcol4 = true
-	rock4x = 470
-	rock4y = 780
-	rock4 = love.graphics.newImage("images/rock.png")
-	rock4hitbox = Collider:addRectangle(rock4x,rock4y,22,22)
-
-	rockcol5 = true
-	rock5x = 200
-	rock5y = 500
-	rock5 = love.graphics.newImage("images/rock.png")
-	rock5hitbox = Collider:addRectangle(rock5x,rock5y,22,22)
-
-	rockcol6 = true
-	rock6x = 1000
-	rock6y = 700
-	rock6 = love.graphics.newImage("images/rock.png")
-	rock6hitbox = Collider:addRectangle(rock6x,rock6y,22,22)
-
-	rockcol7 = true
-	rock7x = 150
-	rock7y = 480
-	rock7 = love.graphics.newImage("images/rock.png")
-	rock7hitbox = Collider:addRectangle(rock7x,rock7y,22,22)
-
-	rockcol8 = true
-	rock8x = 340
-	rock8y = 650
-	rock8 = love.graphics.newImage("images/rock.png")
-	rock8hitbox = Collider:addRectangle(rock8x,rock8y,22,22)
-
-	rockcol9 = true
-	rock9x = 660
-	rock9y = 700
-	rock9 = love.graphics.newImage("images/rock.png")
-	rock9hitbox = Collider:addRectangle(rock9x,rock9y,22,22)
-
-	rockcol10 = true
-	rock10x = 940
-	rock10y = 40
-	rock10 = love.graphics.newImage("images/rock.png")
-	rock10hitbox = Collider:addRectangle(rock10x,rock10y,22,22)
-
-	rockcol11 = true
-	rock11x = 1100
-	rock11y = 780
-	rock11 = love.graphics.newImage("images/rock.png")
-	rock11hitbox = Collider:addRectangle(rock11x,rock11y,22,22)
-
-	rockcol12 = true
-	rock12x = 230
-	rock12y = 850
-	rock12 = love.graphics.newImage("images/rock.png")
-	rock12hitbox = Collider:addRectangle(rock12x,rock12y,22,22)
-
-	mouse12 = Collider:addRectangle(400,500, 1, 1)
-
-
-	playerhitbox = Collider:addRectangle(game.PlayerX, game.PlayerY,16,16)
-
-
-	ShipX = 270
-	ShipY = 890
-	Ship = love.graphics.newImage("images/ship1.png")
-	shiphitbox = Collider:addRectangle(ShipX,ShipY,78,48)
-
-
-
-
-
-	GameMouseBackArea = false
-	GameMouseDetect = 0
-	GameMouseOnBtn = false
-
-
-
-
-
-
-=======
 	Collider = HC(300, on_collision , collision_stop)
 	
 	mouse12 = Collider:addPoint(game.PlayerX, game.PlayerY)
@@ -358,7 +231,6 @@ function game:init()
 	ShipYours = false
 	ExitShip = false
 	shipdead = false
->>>>>>> Thomas-Changes
 end
 
 
@@ -367,44 +239,19 @@ end
 
 
 function game:keypressed(key)
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> Thomas-Changes
 	if key == "escape" and paused == false then
    		paused = true
    		ht = false
    		love.mouse.setCursor(cursor)
   	end
 
-<<<<<<< HEAD
-
-
-
-
-  	if key =="return" and welcome == true then
-=======
   	if key == "return" and welcome == true then
->>>>>>> Thomas-Changes
   		Restart = false
   		welcome = false
   		love.mouse.setCursor(crosshair)
   		love.audio.play(EnterGame)
   	end
 
-<<<<<<< HEAD
-
-
-=======
   	if key == "return" and gameover == true then
   		love.audio.play(EnterGame)
     	Gamestate.switch(menu)
@@ -413,7 +260,6 @@ function game:keypressed(key)
    		love.audio.stop(GameMusic)
     	Restart = true
   	end
->>>>>>> Thomas-Changes
 
     if key == "x" and zoomcam == false then
 		game.Cam:zoom(2)
@@ -425,20 +271,6 @@ function game:keypressed(key)
 		zoomcam = false
 	end
 
-<<<<<<< HEAD
-
-
-
-	if key == "e" and shipyes == true then
-		PlayerSpeed = 250
-	end
-
-
-
-end
-
-
-=======
 	if key == "e" and ShipYes == true then
 		InShip = true
 		ShipYours = true
@@ -457,7 +289,6 @@ end
 
 
 
->>>>>>> Thomas-Changes
 function game:mousepressed(mx, my, button)
 
 	-- Gun
@@ -473,22 +304,14 @@ function game:mousepressed(mx, my, button)
 	end
 
 	-- Gun Sounds
-<<<<<<< HEAD
-	if button == "l" and game.GunShot == true and welcome == false then
-=======
 	if button == "l" and game.GunShot == true and welcome == false and gameover == false and InShip == false then
->>>>>>> Thomas-Changes
 		love.audio.play(game.GunSound)
 		love.audio.stop(game.GunSound1)
 		game.GunShot1 = false
 	end
 
 	-- Gun Sounds
-<<<<<<< HEAD
-	if button == "l" and game.GunShot1 == false and welcome == false then
-=======
 	if button == "l" and game.GunShot1 == false and welcome == false and gameover == false and InShip == false then
->>>>>>> Thomas-Changes
 		love.audio.play(game.GunSound1)
 		love.audio.stop(game.GunSound)
 		game.GunShot = true
@@ -505,8 +328,6 @@ function game:mousepressed(mx, my, button)
   		love.audio.play(EnterGame)
   	end
 
-<<<<<<< HEAD
-=======
   	if button == "l" and GameOverMouseBackArea == true and gameover == true then
 		love.audio.play(EnterGame)
     	Gamestate.switch(menu)
@@ -515,7 +336,6 @@ function game:mousepressed(mx, my, button)
    		love.audio.stop(GameMusic)
     	Restart = true
   	end
->>>>>>> Thomas-Changes
 
 
 end
@@ -529,68 +349,6 @@ function game:update(dt)
 	------ CAMERA ------
 
 	------ PLAYER MOVEMENT ------
-<<<<<<< HEAD
-	if love.keyboard.isDown("a") then
-		game.PlayerX = game.PlayerX - PlayerSpeed * dt
-    end
-
-	if love.keyboard.isDown("d") then
-		game.PlayerX = game.PlayerX + PlayerSpeed * dt
-    end
-
-	if love.keyboard.isDown("w") then
-		game.PlayerY = game.PlayerY - PlayerSpeed * dt
-    end
-
-	if love.keyboard.isDown("s") then
-		game.PlayerY = game.PlayerY + PlayerSpeed * dt
-    end
-
-
-
-
-
-
-
-    SprintTime = SprintTime + dt
-
-    if love.keyboard.isDown("lshift") then
-		PlayerSpeed = 130
-		Sprint = true
-    end
-
-    if not love.keyboard.isDown("lshift") then
-		PlayerSpeed = 70
-		Sprint = false
-    end
-
-    if Sprint == true and SprintTime > 5 then
-    	Tired = true
-    	PlayerSpeed = 70
-    end
-
-    if Sprint == false then
-    	SprintTime = 0
-    end
-
-    if Tired == true and Sprint == false then
-    	PlayerSpeed = 70
-    	TiredTime = TiredTime + dt
-    	SprintTime = 0
-    end
-
-    if TiredTime > 1.7 then
-    	Tired = false
-    	TiredTime = 0
-    end
-
-    if Tired == true and Sprint == true then
-    	PlayerSpeed = 70
-    	SprintTime = 0
-    	TiredTime = 0
-    end
-
-=======
 	if love.keyboard.isDown("a") and InShip == false then
 		game.PlayerX = game.PlayerX - PlayerSpeed * dt
     end
@@ -651,7 +409,6 @@ function game:update(dt)
     	TiredTime = 0
     end
 
->>>>>>> Thomas-Changes
 
 
 
@@ -685,81 +442,6 @@ function game:update(dt)
 
 
 
-<<<<<<< HEAD
-
-	if rockhitbox:collidesWith(mouse12) then
-    	Collider:remove(rockhitbox)
-    	rockcol = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock2hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock2hitbox)
-    	rockcol2 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock3hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock3hitbox)
-    	rockcol3 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock4hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol4 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock5hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol5 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock6hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol6 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock7hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol7 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock8hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol8 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock9hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol9 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock10hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol10 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock11hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol11 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-	if rock12hitbox:collidesWith(mouse12) then
-    	Collider:remove(rock4hitbox)
-    	rockcol12 = false
-    	--table.remove(game.Bullets, i)
-	end
-
-=======
 	if mouse12:collidesWith(rockbox1) and rockdestroyed1 == false then
 		rockhit1 = true
     elseif not mouse12:collidesWith(rockbox1) then
@@ -1130,21 +812,10 @@ function game:update(dt)
 		shiphealth = shiphealth - 1
 		ShipHealthX = ShipHealthX - 30 * dt
 	end
->>>>>>> Thomas-Changes
 
 
 
 
-<<<<<<< HEAD
-	if shiphitbox:collidesWith(playerhitbox) then
-    	--shipyes = true
-    	PlayerSpeed = 400
-    	ShipX = game.PlayerX
-    	ShipY = game.PlayerY
-		for i, o in ipairs(game.Bullets) do
-		o.Speed = 900
-		end
-=======
 
 	if playerhealth == 0 then
 		gameover = true
@@ -1152,15 +823,12 @@ function game:update(dt)
 
 	if shiphealth == 0 then
 		shipdead = true
->>>>>>> Thomas-Changes
 	end
 
 
 
 
 
-<<<<<<< HEAD
-=======
 
   
 
@@ -1245,7 +913,6 @@ function game:update(dt)
 
 
 
->>>>>>> Thomas-Changes
     Collider:update(dt)
 
 
@@ -1267,74 +934,6 @@ function game:update(dt)
 
 
 	if Restart == true then
-<<<<<<< HEAD
-	welcome = true
-	zoomcam = false
-	paused = false
-	game.PlayerY = 400
-	game.PlayerX = 800
-	game.ShotTime = 0
-	game.ShotTimePlus = 0
-	game.Bullets = { }
-	game.GunShot = false
-	game.GunShot1 = false
-	game.Cam = camera(game.PlayerX, game.PlayerY, 2.5)
-	Tired = false
-	TiredTime = 0 
-	Sprint = false
-	PlayerSpeed = 70
-	SprintTime = 0
-	Collider = HC(100,on_collision , collision_stop)
-	rockcol = true
-	rockx = 800
-	rocky = 300
-	rockhitbox = Collider:addRectangle(rockx,rocky,22,22)
-	rockcol2 = true
-	rock2x = 500
-	rock2y = 350
-	rock2hitbox = Collider:addRectangle(rock2x,rock2y,22,22)
-	rockcol3 = true
-	rock3x = 100
-	rock3y = 900
-	rock3hitbox = Collider:addRectangle(rock3x,rock3y,22,22)
-	rockcol4 = true
-	rock4x = 470
-	rock4y = 780
-	rock4hitbox = Collider:addRectangle(rock4x,rock4y,22,22)
-	rockcol5 = true
-	rock5x = 200
-	rock5y = 500
-	rock5hitbox = Collider:addRectangle(rock5x,rock5y,22,22)
-	rockcol6 = true
-	rock6x = 1000
-	rock6y = 700
-	rock6hitbox = Collider:addRectangle(rock6x,rock6y,22,22)
-	rockcol7 = true
-	rock7x = 150
-	rock7y = 480
-	rock7hitbox = Collider:addRectangle(rock7x,rock7y,22,22)
-	rockcol8 = true
-	rock8x = 340
-	rock8y = 650
-	rock8hitbox = Collider:addRectangle(rock8x,rock8y,22,22)
-	rockcol9 = true
-	rock9x = 660
-	rock9y = 700
-	rock9hitbox = Collider:addRectangle(rock9x,rock9y,22,22)
-	rockcol10 = true
-	rock10x = 940
-	rock10y = 40
-	rock10hitbox = Collider:addRectangle(rock10x,rock10y,22,22)
-	rockcol11 = true
-	rock11x = 1100
-	rock11y = 780
-	rock11hitbox = Collider:addRectangle(rock11x,rock11y,22,22)
-	rockcol12 = true
-	rock12x = 230
-	rock12y = 850
-	rock12hitbox = Collider:addRectangle(rock12x,rock12y,22,22)
-	mouse12 = Collider:addRectangle(400,500, 1, 1)
-=======
 		game.PlayerY = 400
 		game.PlayerX = 800
 		game.ShotTime = 0
@@ -1462,7 +1061,6 @@ function game:update(dt)
 		ShipYours = false
 		ExitShip = false
 		shipdead = false
->>>>>>> Thomas-Changes
 	end
 
 
@@ -1510,10 +1108,6 @@ function game:update(dt)
 
 
 
-<<<<<<< HEAD
-
-
-=======
 	if GameOverMouseOnBtn == false and gameover == true then
 		GameOverMouseDetect = 0
 		love.audio.stop(GameSelect1M)
@@ -1564,31 +1158,11 @@ function love.focus(f)
 		ht = false
    		love.mouse.setCursor(cursor)
 	end
->>>>>>> Thomas-Changes
 end
 
 
 
 
-<<<<<<< HEAD
-	
-
-
-
-
-function love.focus(f)
-	if not f and not paused and not welcome then
-	paused = true
-	ht = false
-   	love.mouse.setCursor(cursor)
-	end
-end
-
-
-
-
-=======
->>>>>>> Thomas-Changes
 
 
 
@@ -1599,12 +1173,6 @@ function game:draw()
 	game.Player:setFilter( 'nearest', 'nearest' )
 	game.Shot:setFilter( 'nearest', 'nearest' )
 
-<<<<<<< HEAD
-
-
-	rock:setFilter( 'nearest', 'nearest' )
-	Ship:setFilter( 'nearest', 'nearest' )
-=======
 	Ship:setFilter( 'nearest', 'nearest' )
 	
 	rock1:setFilter( 'nearest', 'nearest' )
@@ -1616,7 +1184,6 @@ function game:draw()
 	rock7:setFilter( 'nearest', 'nearest' )
 	rock8:setFilter( 'nearest', 'nearest' )
 	rock9:setFilter( 'nearest', 'nearest' )
->>>>>>> Thomas-Changes
 
 
 
@@ -1635,97 +1202,14 @@ function game:draw()
 	for i, o in ipairs(game.Bullets) do
 		love.graphics.draw(game.Shot, o.x, o.y, o.Dir)
 		mouse12:moveTo(o.x, o.y)
-<<<<<<< HEAD
-		--mouse12:draw('line')
-		--mouse12:setRotation(o.Dir - 300)
-	end
-	-- Player
-	love.graphics.draw(game.Player, game.PlayerX, game.PlayerY, PlayerRot, 1, 1, game.Player:getWidth()/2, game.Player:getHeight()/2)
-	
-=======
 	end
 	-- Player
 	
 	
->>>>>>> Thomas-Changes
 
 
 
 
-<<<<<<< HEAD
-	if rockcol == true then
-	love.graphics.draw(rock, rockx, rocky)
-    --rockhitbox:draw('line')
-    end
-
-    if rockcol2 == true then
-    love.graphics.draw(rock, rock2x, rock2y)
-    --rock2hitbox:draw('line')
-    end
-
-    if rockcol3 == true then
-    love.graphics.draw(rock, rock3x, rock3y)
-    --rock3hitbox:draw('line')
-    end
-
-    if rockcol4 == true then
-    love.graphics.draw(rock, rock4x, rock4y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol5 == true then
-    love.graphics.draw(rock, rock5x, rock5y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol6 == true then
-    love.graphics.draw(rock, rock6x, rock6y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol7 == true then
-    love.graphics.draw(rock, rock7x, rock7y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol8 == true then
-    love.graphics.draw(rock, rock8x, rock8y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol9 == true then
-    love.graphics.draw(rock, rock9x, rock9y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol10 == true then
-    love.graphics.draw(rock, rock10x, rock10y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol11 == true then
-    love.graphics.draw(rock, rock11x, rock11y)
-    --rock4hitbox:draw('line')
-	end
-
-	if rockcol12 == true then
-    love.graphics.draw(rock, rock12x, rock12y)
-    --rock4hitbox:draw('line')
-	end
-
-
-
-
-	love.graphics.draw(Ship, ShipX, ShipY)
-	shiphitbox:draw('line')
-
-	playerhitbox:draw('line')
-	playerhitbox:moveTo(game.PlayerX, game.PlayerY)
-	shiphitbox:moveTo(ShipX, ShipY)
-
-
-
-=======
 
 
 	if InShip == true then
@@ -1845,7 +1329,6 @@ function game:draw()
 		rockbox9:moveTo(rockx9, rocky9)
 	end
 	
->>>>>>> Thomas-Changes
 
 
 
@@ -1872,21 +1355,6 @@ function game:draw()
 	love.graphics.rectangle("fill", 107, 5, 3, 18 )
 	love.graphics.print('Health', 23, 5)
 
-<<<<<<< HEAD
-    if welcome == true then
-
-    	love.graphics.rectangle("fill", 185, 55, 910, 510 )
-
-    	love.graphics.setColor(0, 0, 0)
-    	love.graphics.rectangle("fill", 190, 60, 900, 500 )
-
-    	love.graphics.setColor(255,255,255)
-    	love.graphics.setFont( welcomefont )
-		
-		love.graphics.rectangle("fill", GameArrowX, BackBtnY + 5, 29, 35 )
-		love.graphics.rectangle("fill", GameArrowX + 291, BackBtnY + 5, 29, 35 )
-
-=======
 
 	if ShipYours == true then
 		love.graphics.setColor(255, 255, 255)
@@ -1941,7 +1409,6 @@ function game:draw()
     	love.graphics.setFont( welcomefont )
 		love.graphics.rectangle("fill", GameArrowX, BackBtnY + 5, 29, 35 )
 		love.graphics.rectangle("fill", GameArrowX + 291, BackBtnY + 5, 29, 35 )
->>>>>>> Thomas-Changes
 		love.graphics.print('Welcome!', 593, 110)
 		love.graphics.print("Thank you for taking the time to test Piquant Interactive's project, Space Game.", 215, 176)
 		love.graphics.print("The current version solely involves a minigame which has you battle inanimate", 215, 194)
@@ -1953,10 +1420,6 @@ function game:draw()
 		love.graphics.print("Gun - Mouse = Aim, LeftClick = Shoot", 215, 338)
 		love.graphics.print("Camera - X = Zoom In, Z = Zoom Out", 215, 356)
 		love.graphics.print("Other - Esc = Pause, LeftShift = Sprint", 215, 374)
-<<<<<<< HEAD
-
-=======
->>>>>>> Thomas-Changes
 		love.graphics.setFont( BackFont )
 		love.graphics.print('Continue', BackBtnX, BackBtnY)
 	end
